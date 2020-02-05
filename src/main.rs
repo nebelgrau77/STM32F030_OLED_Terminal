9cba119091c539e88d4b031d139e0f9ec3a6b1bc
Michal <36729174+nebelgrau77@users.noreply.github.com>
2020-02-05 09:15:44 +0100

    Update main.rs
    some clean-up

--- src/main.rs
+++ src/main.rs
@@ -19,43 +19,33 @@ use crate::hal::{
     i2c::I2c
 };
 
-
-
 #[entry]
 fn main() -> ! {
     
-
     if let (Some(mut p), Some(_cp)) = (stm32::Peripherals::take(),cortex_m::peripheral::Peripherals::take()) {
         
         cortex_m::interrupt::free(move |cs| {
 
         let mut rcc = p.RCC.configure().sysclk(8.mhz()).freeze(&mut p.FLASH);
-      
-        
+              
         let gpioa = p.GPIOA.split(&mut rcc);
         let scl = gpioa.pa9.into_alternate_af4(cs);
         let sda = gpioa.pa10.into_alternate_af4(cs);
         let i2c = I2c::i2c1(p.I2C1, (scl, sda), 400.khz(), &mut rcc);
 
-
         // Set up the display
         let mut disp: TerminalMode<_> = SSD1306Builder::new().with_size(DisplaySize).connect_i2c(i2c).into();
         
         disp.init().unwrap();
-            
         disp.flush().unwrap();
         
         for c in 97..123 {
             disp.write_str(unsafe { core::str::from_utf8_unchecked(&[c]) }).unwrap(); 
         }
-        
-        
+               
     
         loop {
-            
-            
-    
-            for c in 97..123 {
+                for c in 97..123 {
                 disp.write_str(unsafe { core::str::from_utf8_unchecked(&[c]) }).unwrap(); 
             }
         }
@@ -64,16 +54,6 @@ fn main() -> ! {
     
 }
 
-    loop {
-        continue;
-    }
-
-    
+    loop {continue;}
     
 }
-
-/*
-#[exception]
-fn HardFault(ef: &ExceptionFrame) -> ! {
-    panic!("{:#?}", ef);
-} */
